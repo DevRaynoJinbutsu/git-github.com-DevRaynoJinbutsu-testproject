450abef079c5acf4b8ca3ff2746342cd3d6aec5d
DevRaynoJinbutsu <kyoto117tsuge@gmail.com>
2018-07-12 12:11:24 +0900

    Trello連携テスト 適当にソース変更してみてどうなるか確認

--- helloworldBase/CPPTest.cpp
+++ helloworldBase/CPPTest.cpp
@@ -10,30 +10,35 @@
 #include "xxtea.h"
 using namespace std;
 
-#define SECURITY_KEY "rHvFaRHC5A"
+#define SECURITY_KEY "czquyc67EP404676tPQFAntSOz9dzvrL19XR4JWk8XZkJC1A346f0vKA6xO0ck8P"
 
 CPPTest::CPPTest()
 {
     printf("コンストラクタ\n");
-    string str = "PHWymibeIqr3TErFZYsbLYYAVC1ntsuXdKTnDcLLSYWfC0LwDjnAtJQzZw8FEIEim443BfFzJ80DnevAkedTHOlljcWLFGbpxyWxnRlNA9jWqgRjXjXnEuonNh0uZsrlTQBNDrHqpWRtFpGoBFnXvRUpaZsMtQ02jwJTLTDewdd2ubeN9VoZjprBNtsas4ereXtiLFBM";
+    string str = "-C880OT/97Zbf/R9!v5x%R0%%e(IuaU&Y)Syd/w74A5x6#7-KY3nsP-Rb)Ivp18-";
     printf("1:%s\n",str.c_str());
-
     
-    size_t len;
+    size_t len = 0;
+    // xxtea暗号化
     unsigned char *encrypt_data = (unsigned char *)xxtea_encrypt(str.c_str(), str.size(), SECURITY_KEY, &len);
 
     string deStr = string((const char*)encrypt_data);
     printf("destr!:%s\n",deStr.c_str());
 
-    size_t len2;
-    char *decrypt_data = (char *)xxtea_decrypt(deStr.c_str(), deStr.size(), SECURITY_KEY, &len2);
-    if (strncmp(str.c_str(), decrypt_data, deStr.size()) == 0)
+    // xxtea復号
+    size_t len2 = 0;
+    const char *decrypt_data = (const char *)xxtea_decrypt(deStr.c_str(), deStr.size(), SECURITY_KEY, &len2);
+    printf("len2:%ld\n",len2);
+    if (len2 > 0)
     {
-        printf("success!:%s len:%ld\n",decrypt_data,len2);
-    }
-    else
-    {
-        printf("failed!\n");
+        if (strncmp(str.c_str(), decrypt_data, deStr.size()) == 0)
+        {
+            printf("success!:%s len:%ld\n",decrypt_data,len2);
+        }
+        else
+        {
+            printf("failed!\n");
+        }
     }
 }
 
